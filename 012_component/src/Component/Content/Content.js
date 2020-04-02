aa3b6a19190cbcb676745a7859e6816585276733
Vuoppa <vuoppa@example.net>
2020-04-02 18:03:24 +0700

    Filancer

--- 012_component/src/Component/Content/Content.js
+++ 012_component/src/Component/Content/Content.js
@@ -5,28 +5,35 @@ class Content extends Component {
         super(props);
 
     }
-
+    thongbao = () => { alert("Cach xu ly tt trong reactJS") };
+    thongbaoso2 = () => { alert("Cach xu ly tt trong react JS222") }
 
     render() {
         return (
 
-            <section>
-                <div className="container">
-                    <div className="row align-items-center">
-                        <div className={"col-lg-6 " + this.props.vitri1}>
-                            <div className="p-5">
-                                <img className="img-fluid rounded-circle" src={this.props.linkanh} alt="" />
-                            </div>
-                        </div>
-                        <div className={"col-lg-6 "}>
-                            <div className="p-5">
-                                <h2 className="display-4">{this.props.tieude}</h2>
-        <p> {this.props.trichdan}</p>
-                            </div>
+            <div className="col-lg-4">
+
+                <div className="row">
+                    <div className={"col-lg-5 "}>
+                        <div className=" p-1">
+                            <img className="img-fluid rounded-circle" src={this.props.linkanh} alt="" />
                         </div>
                     </div>
+                    <div className={"col-lg-7 "}>
+                        <div className=" p-5">
+                            <h2 className="display-6">{this.props.tieude}</h2>
+                            <p> {this.props.trichdan}</p>
+                            <div className="row">
+                                <div className=" btn">
+                                    <div className="btn btn-block btn-info" onClick={this.thongbao}>Remove</div>
+                                    <div className="btn btn-block btn-warning" onClick={this.thongbaoso2}>Modify</div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
-            </section>
+            </div>
 
         );
     }
