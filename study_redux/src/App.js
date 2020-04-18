5b173b172f172749bf266d436da9c0bd3f0149a5
Vuoppa <vuoppa@example.net>
2020-04-18 09:58:12 +0700

    Gioi thieu ve reducer trong redux

--- study_redux/src/App.js
+++ study_redux/src/App.js
@@ -4,34 +4,56 @@ import React, { Component } from 'react';
 class App extends Component {
 
   render() {
-    /*     var p1 = {
-          sanpham: "tivi",
-          manhinh: 30
-        }
-        // var p2 = p1;//gia tri bang nhau. p2 t.doi thi p1 t.doi
-        var p2 = { ...p1 }
-        console.log(p1);
-        p2.manhinh = 40;
-        console.log(p2);
-        console.log(p1);
-     */
-    /*     var a1 = [1, 2, 3, 4, 5];
-        var a2 = [...a1];
-        a2[0] = 100;
-        console.log(a2);
-        console.log(a1);
-     */
-    var b1 = {
-      num: [23, 56, 89],
-      status: true
+    var redux = require('redux');
+    var oldState = {
+      num: ['manhinh', 'chuot', 'banphim'],
+      editStatus: true
     }
-    var b2 = { ...b1, status: false, num: [23, 78, 987] }
-    // b2.status = false;
-    console.log(b2);
-    var b3 = { ...b1, num: [...b1.num, 100] };
-    // b3.num[3] = 100;
-    console.log(b3);
 
+    var reducer1 = (state = oldState, action) => {
+      switch (action.type) {
+        case 'CHANGE_EDIT_STATUS':
+          return { ...state, editStatus: !state.editStatus };
+        // break;
+
+        case 'ADD_NEW':
+          return { ...state, num: [...state.num, action.newItem] };
+        // break;
+
+        case 'DELETE':
+          return { ...state, num: [...state.num.filter((val, key) => key !== action.index)] };
+        // break;
+
+        default: return state;
+        // break;
+      }
+
+
+    }
+
+
+    var store1 = redux.createStore(reducer1);
+
+    store1.subscribe(() => {
+      console.log('state thay doi');
+      console.log(store1.getState());
+    })
+
+    // store1.subscribe
+
+    store1.dispatch({
+      type: "CHANGE_EDIT_STATUS"
+    });
+
+    store1.dispatch({
+      type: "ADD_NEW",
+      newItem: "tainghe"
+    });
+
+    store1.dispatch({
+      type: 'DELETE',
+      index: 0
+    })
 
     return (
       <div>
